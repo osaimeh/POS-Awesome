c7f716e84445db926c5b0a31f9218dae8e7ea854
Youssef Restom <youssefrestom@gmail.com>
2021-05-07 21:27:03 +0000

    feat: autofocus on new customer

--- posawesome/public/js/posapp/components/pos/NewCustomer.vue
+++ posawesome/public/js/posapp/components/pos/NewCustomer.vue
@@ -14,6 +14,7 @@
               <v-col cols="12">
                 <v-text-field
                   dense
+                  autofocus
                   color="indigo"
                   label="الإسم"
                   background-color="white"
